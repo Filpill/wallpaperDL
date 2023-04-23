7dda1fc5ee3819f2b0a7613a2a1e06bc657a89da
Filpill <filiplivancic@gmail.com>
2023-04-23 14:46:18 +0000

    added python script

---
+++ wallpaperDL.py
@@ -0,0 +1,31 @@
+import os
+import string
+import random
+import requests
+
+def wallpaper_search_api(query):
+    url = f"https://wallhaven.cc/api/v1/search?q={query}"
+    res = requests.get(url)
+    json_data = res.json()
+    dl_links = [wallpaper["path"] for wallpaper in json_data["data"]]
+    return dl_links
+
+def generate_id():
+    return ''.join(random.choices(string.ascii_lowercase + string.digits, k=6))
+
+def download_wallpaper(url):
+    print(f"Downloading...{url}")
+    user="filpill"
+    res = requests.get(url)
+    wall_name = generate_id()
+    ext = os.path.splitext(url)[1]
+    dl_path = f"/home/{user}/wallpapers/{wall_name}{ext}"
+    open(dl_path,'wb').write(res.content)
+
+# Search Query
+query = "winter"
+wall_dl_url = wallpaper_search_api(query)
+
+# Download Wallpapers Based on Search Query
+for url in wall_dl_url:
+    download_wallpaper(url)
